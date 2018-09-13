4862f22d355a9f79042f948d56d839dcbe7aa45a
Adam Fieldson <atfieldson@gmail.com>
2018-09-13 11:05:13 -0500

    started ng

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,8 @@
 console.log('js');
+
+const shellApp = angular.module('ShellApp', [] )  //the empty array will be filled with dependencies in the future
+
+shellApp.controller('ShellController', [function(){
+    let self = this;
+    self.number = 7;
+}])
